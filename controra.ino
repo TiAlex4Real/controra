fdf13d65d1d321b9cacd94e41503a046dbc26f07
Alexander Tikunov <tialex4real@gmail.com>
2022-03-26 17:25:09 +0300

    Remove serial port prints, minor refactor

--- controra.ino
+++ controra.ino
@@ -1,42 +1,48 @@
 #include <Keyboard.h>
 
-// Define how many pins to use as buttons
-#define BUTTONS_TOTAL 2
-// Anti-jitter counter max value (lower = faster, higher = better anti-jitter)
-#define JITTER_MAX_VALUE 5
-
 typedef struct {
   int pin;
   int button;
 } t_button_pin_conf;
 
 typedef struct {
-  int jitter;
+  int debounce;
   int pressed;
 } t_button_state;
 
+// Define how many pins to use as buttons
+#define BUTTONS_TOTAL 9
+// Debounce counter max value (lower = faster, higher = better debounce)
+#define DEBOUNCE_MAX_VALUE 5
 
-// Define button mappings here. See keys macro in Keyboard.h.
+// Define board pin <-> button mappings here. See keys macro in Keyboard.h.
 t_button_pin_conf button_pin_conf[BUTTONS_TOTAL] = {
-  {2, KEY_LEFT_ARROW},
-  {3, KEY_RIGHT_ARROW}
+  {0, 'w'},
+  {1, 'a'},
+  {2, 's'},
+  {3, 'd'},
+  {4, 'u'},
+  {5, 'i'},
+  {6, 'o'},
+  {7, 'j'},
+  {8, 'k'}
 };
 
 t_button_state button_state[BUTTONS_TOTAL];
 
-inline void jitter_inc(int index) {
-  if (button_state[index].jitter < JITTER_MAX_VALUE) button_state[index].jitter++;
+inline void debounce_inc(int index) {
+  if (button_state[index].debounce < DEBOUNCE_MAX_VALUE) button_state[index].debounce++;
 }
 
-inline void jitter_dec(int index) {
-  if (button_state[index].jitter > 0 ) button_state[index].jitter--;
+inline void debounce_dec(int index) {
+  if (button_state[index].debounce > 0) button_state[index].debounce--;
 }
 
 void setup_buttons() {
   for (int i = 0; i < BUTTONS_TOTAL; ++i) {
     pinMode(button_pin_conf[i].pin, INPUT_PULLUP);
     digitalWrite(button_pin_conf[i].pin, HIGH);
-    button_state[i].jitter = 0;
+    button_state[i].debounce = 0;
     button_state[i].pressed = false;
   }  
 }
@@ -45,28 +51,22 @@ void setup_buttons() {
 void setup() {
   setup_buttons();
   Keyboard.begin();
-  // Serial is for debugging purposes only
-  Serial.begin(9600);
 }
 
 void loop()
 {
   for (int i = 0; i < BUTTONS_TOTAL; ++i) {
     if (!digitalRead(button_pin_conf[i].pin)) {
-      jitter_inc(i);
-      if (button_state[i].jitter == JITTER_MAX_VALUE && button_state[i].pressed == false) {
+      debounce_inc(i);
+      if (button_state[i].debounce == DEBOUNCE_MAX_VALUE && button_state[i].pressed == false) {
         button_state[i].pressed = true;
         Keyboard.press(button_pin_conf[i].button);
-        Serial.write("press ");
-        Serial.println(button_pin_conf[i].button);
       }
     } else {
-      jitter_dec(i);
-      if (button_state[i].jitter == 0 && button_state[i].pressed == true) {
+      debounce_dec(i);
+      if (button_state[i].debounce == 0 && button_state[i].pressed == true) {
         button_state[i].pressed = false;
         Keyboard.release(button_pin_conf[i].button);
-        Serial.write("release ");
-        Serial.println(button_pin_conf[i].button);
       }
     }
   }
