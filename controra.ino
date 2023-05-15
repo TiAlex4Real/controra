cedf256550c3f377e076fa99db010e0e37969df7
Alexander Tikunov <tialex4real@gmail.com>
2023-05-16 00:03:45 +0300

    Remade directions buffer to millis() call
    Also:
     - Improved debuging prints to events instead if loop
     - Additional comments

--- controra.ino
+++ controra.ino
@@ -1,35 +1,41 @@
 #include <Keyboard.h>
 
-// Uncomment to use serial port logging
+// Uncomment to use serial port logging instead of real keyboard presses
 // #define DEBUG
 
 // Directions buffer defines. Used to prohibit simultaneous presses
 // of two opposite directions (up <-> down, left <-> right).
-#define DIRECTIONS_TOTAL 3
-enum DIRECTION_TYPE {DIRECION_NONE, DIRECTION_HORIZONTAL, DIRECTION_VERTICAL};
-#define DIRECTION_BUFFER_CONTENT_CLEAR 0 // buffer content when it's empty (pressing of any direction is allowed)
-#define DIRECTION_BUFFER_DELAY_MAX_VALUE 100 // number of cycles when it's not allowed to put anything into buffer
+#define DIRECTIONS_TOTAL 2
+enum DIRECTION_TYPE {DIRECION_NONE = -1, DIRECTION_HORIZONTAL, DIRECTION_VERTICAL};
+
+// Number of milliseconds when it's not allowed to press direction button after previous
+// was released. 17 is minimal whole number that is higher than 16.667 miliiseconds (1 game frame).
+// Seems to be enough for not to trigger instant input jumps to opposite direction in GGST.
+#define DIRECTION_BUFFER_DELAY_MS 17
 
 typedef struct {
-  int content;
-  int delay;
+  int content; // key value in buffer ('w', 'a', KEY_RETURN, etc).
+  unsigned long clear_millis; // value of millis() call when buffered button was released
 } t_direction_buffer_state;
+
+#define DIRECTION_BUFFER_CONTENT_CLEAR 0 // buffer content when it's empty (pressing of any direction is allowed)
 
 // Common buttons defines.
 // Define how many pins to use as buttons
 #define BUTTONS_TOTAL 13
-// Debounce counter max value (lower = faster, higher = better debounce)
+// Debounce counter max value (lower = faster, higher = better debounce).
+// Icrease it if the output flicker.
 #define DEBOUNCE_MAX_VALUE 3
 
 typedef struct {
-  int pin;
-  int button;  
+  int pin; // pin number on board
+  int button; // button value 
   DIRECTION_TYPE direction_type;
 } t_button_pin_conf;
 
 typedef struct {
-  int debounce;
-  int pressed;
+  int debounce; // current value of debounce counter
+  bool pressed; // pressed state
 } t_button_state;
 
 // Button mapping defines.
@@ -37,7 +43,7 @@ typedef struct {
 t_button_pin_conf button_pin_conf[BUTTONS_TOTAL] = {
   {0, 'w', DIRECTION_VERTICAL},
   {1, 'j', DIRECION_NONE},
-  {2, 'z', DIRECION_NONE},
+  {2, ' ', DIRECION_NONE},
   {3, KEY_RETURN, DIRECION_NONE},
   {4, 't', DIRECION_NONE},
   {5, 'o', DIRECION_NONE},
@@ -63,33 +69,63 @@ inline void debounce_dec(int index) {
   if (button_state[index].debounce > 0) button_state[index].debounce--;
 }
 
+// Main button handler
 inline void button_handle(int button_offset, bool pressed) {
   if (pressed) {
-      button_state[button_offset].pressed = true;
-      #ifndef DEBUG
-      Keyboard.press(button_pin_conf[button_offset].button);
-      #endif
+    button_state[button_offset].pressed = true;
+    #ifndef DEBUG
+    Keyboard.press(button_pin_conf[button_offset].button);
+    #else
+    Serial.print((char)button_pin_conf[button_offset].button);
+    Serial.print(".\n");
+    #endif
   } else {
-      button_state[button_offset].pressed = false;
-      #ifndef DEBUG
-      Keyboard.release(button_pin_conf[button_offset].button);
-      #endif
+    button_state[button_offset].pressed = false;
+    #ifndef DEBUG
+    Keyboard.release(button_pin_conf[button_offset].button);
+    #else
+    Serial.print((char)button_pin_conf[button_offset].button);
+    Serial.print("^\n");
+    #endif
   }
 }
 
-void setup_buttons() {
-  for (int i = 0; i < BUTTONS_TOTAL; ++i) {
-    pinMode(button_pin_conf[i].pin, INPUT_PULLUP);
-    digitalWrite(button_pin_conf[i].pin, HIGH);
-    button_state[i].debounce = 0;
-    button_state[i].pressed = false;
-  }  
+// Directions buffer handler. Used in combination with button_handle().
+// Returns true if it's allowed to press or release button at the moment.
+bool directions_buffer_handle(int button_offset, bool pressed) {
+  static int direction_type, button;
+  static t_direction_buffer_state *direction_buffer_state_loc;
+  static unsigned long current_millis;
+  
+  direction_type = button_pin_conf[button_offset].direction_type;
+  button = button_pin_conf[button_offset].button;
+  if (direction_type != DIRECION_NONE) {
+    direction_buffer_state_loc = &direction_buffer_state[direction_type];
+    current_millis = millis();
+    if (pressed) {
+      if (direction_buffer_state_loc->content == DIRECTION_BUFFER_CONTENT_CLEAR && \
+          current_millis - direction_buffer_state_loc->clear_millis > DIRECTION_BUFFER_DELAY_MS) {
+        direction_buffer_state_loc->content = button;
+        return true;
+      }
+    } else {
+      if (direction_buffer_state_loc->content == button) {
+        direction_buffer_state_loc->clear_millis = current_millis;
+        direction_buffer_state_loc->content = DIRECTION_BUFFER_CONTENT_CLEAR;
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 void setup() {
   memset(direction_buffer_state, 0, sizeof(t_direction_buffer_state) * DIRECTIONS_TOTAL);
   memset(button_state, 0, sizeof(t_button_state) * BUTTONS_TOTAL);
-  setup_buttons();
+  for (int i = 0; i < BUTTONS_TOTAL; ++i) {
+    pinMode(button_pin_conf[i].pin, INPUT_PULLUP);
+    digitalWrite(button_pin_conf[i].pin, HIGH);
+  }
   Keyboard.begin();
   #ifdef DEBUG
   Serial.begin(115200);
@@ -99,21 +135,13 @@ void setup() {
 void loop()
 {
   for (int i = 0; i < BUTTONS_TOTAL; ++i) {
-    if (direction_buffer_state[button_pin_conf[i].direction_type].content == DIRECTION_BUFFER_CONTENT_CLEAR && \
-        direction_buffer_state[button_pin_conf[i].direction_type].delay > 0) {
-      direction_buffer_state[button_pin_conf[i].direction_type].delay--;
-      if (direction_buffer_state[button_pin_conf[i].direction_type].delay == 0)
-      continue;
-    }
     if (!digitalRead(button_pin_conf[i].pin)) {
       debounce_inc(i);
       if (button_state[i].debounce == DEBOUNCE_MAX_VALUE && button_state[i].pressed == false) {
         if (button_pin_conf[i].direction_type == DIRECION_NONE) {
           button_handle(i, true);
         } else {
-          if (direction_buffer_state[button_pin_conf[i].direction_type].content == DIRECTION_BUFFER_CONTENT_CLEAR && \
-              direction_buffer_state[button_pin_conf[i].direction_type].delay == 0) {
-            direction_buffer_state[button_pin_conf[i].direction_type].content = button_pin_conf[i].button;
+          if (directions_buffer_handle(i, true)) {
             button_handle(i, true);
           }
         }
@@ -124,29 +152,11 @@ void loop()
         if (button_pin_conf[i].direction_type == DIRECION_NONE) {
           button_handle(i, false);
         } else {
-          if (direction_buffer_state[button_pin_conf[i].direction_type].content == button_pin_conf[i].button) {
-            direction_buffer_state[button_pin_conf[i].direction_type].content = DIRECTION_BUFFER_CONTENT_CLEAR;
-            direction_buffer_state[button_pin_conf[i].direction_type].delay = DIRECTION_BUFFER_DELAY_MAX_VALUE;
+          if (directions_buffer_handle(i, false)) {
             button_handle(i, false);
           }
         }
       }
     }
   }
-  #ifdef DEBUG
-  Serial.print("PRESSED: [");
-  for (int i=0; i<BUTTONS_TOTAL; i++) {
-    Serial.print(button_state[i].pressed ? (char)button_pin_conf[i].button : ' ');
-  }
-  Serial.print("] HOR: [");
-  Serial.print((char)direction_buffer_state[DIRECTION_HORIZONTAL].content);
-  Serial.print(' ');
-  Serial.print(direction_buffer_state[DIRECTION_HORIZONTAL].delay);
-  Serial.print("] VER: [");
-  Serial.print((char)direction_buffer_state[DIRECTION_VERTICAL].content);
-  Serial.print(' ');
-  Serial.print(direction_buffer_state[DIRECTION_VERTICAL].delay);
-  Serial.print("]\n");
-  delay(100);
-  #endif
 }
